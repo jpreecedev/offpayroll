d7587a0551b9f9f21764450daad68d8cf4ad7a6d
Jon Preece <jonpreece@hotmail.co.uk>
2020-02-08 16:51:00 +0000

    feat(alerts): added bootstrap style alert messages to agents and consultancies

--- OffPayroll/View/NotificationView.swift
+++ OffPayroll/View/NotificationView.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+@IBDesignable
 class NotificationView: UIView {
-
+    
+    @IBInspectable var bgColor: UIColor = UIColor(red: 209/255, green:228/255, blue:253/255, alpha: 1) {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var viewBorderColor: UIColor = UIColor(red: 209/255, green:228/255, blue:253/255, alpha: 1) {
+        didSet {
+            self.layer.borderColor = viewBorderColor.cgColor
+        }
+    }
+    
     override func draw(_ rect: CGRect) {
         self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor(red: 184/255, green: 218/255, blue: 255/255, alpha: 1).cgColor
         self.layer.cornerRadius = 4
         self.layer.masksToBounds = true
     }
