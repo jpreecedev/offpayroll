0e2041cda70b690b35cd3b5fbcc749d5b0847f3c
Jon Preece <jonpreece@hotmail.co.uk>
2020-02-11 08:48:57 +0000

    feat(comments): agent comments

--- OffPayroll/Controllers/AgentDetailsVC.swift
+++ OffPayroll/Controllers/AgentDetailsVC.swift
@@ -9,15 +9,20 @@
 import UIKit
 import Alamofire
 
-typealias AgentDetailsAPIRequestCompletion = (_ errMsg: String?, _ data: Dictionary<String, AnyObject>) -> Void
+typealias AgentDetailsAPIRequestCompletion = (_ errMsg: String?, _ data: AgentDetailsResult) -> Void
 
-class AgentDetailsVC: UIViewController {
+struct AgentDetailsResult {
+    var fairAgent: FairAgent
+    var comments: [Comment]
+}
+
+class AgentDetailsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var backBtn: UIButton!
     @IBOutlet weak var companyLogo: UIImageView!
     @IBOutlet weak var companyName: UILabel!
     @IBOutlet weak var segmentCtrl: UISegmentedControl!
-
+    
     @IBOutlet weak var specialismsHeaderLabel: UILabel!
     @IBOutlet weak var specialismsLabel: UILabel!
     @IBOutlet weak var locationsHeaderLabel: UILabel!
@@ -33,8 +38,12 @@ class AgentDetailsVC: UIViewController {
     @IBOutlet weak var approachHeaderLabel: UILabel!
     @IBOutlet weak var approachLabel: UILabel!
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     private var _agent: FairAgent!
     var indicator = UIActivityIndicatorView()
+    var comments = [Comment]()
     
     var agent: FairAgent {
         get {
@@ -58,9 +67,17 @@ class AgentDetailsVC: UIViewController {
         
         companyName.text = _agent.name
         companyLogo.image = _agent.image
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.layoutMargins = UIEdgeInsets.zero
+        tableView.separatorInset = UIEdgeInsets.zero
         
         getDataFromAPI(url: URL(string: "https://offpayroll.org.uk/api/agents/\(_agent.name.replacingOccurrences(of: " ", with: "%20"))")!) { (err, data) in
-            self.agent = FairAgentMapper.mapFromAPI(data: data as AnyObject, existingAgent: self._agent)
+            self.agent = data.fairAgent
+            self.comments = data.comments
+            
+            self.tableView.reloadData()
             
             self.companyLogo.image = self.agent.image
             self.companyName.text = self.agent.name
@@ -83,11 +100,33 @@ class AgentDetailsVC: UIViewController {
     
     @IBAction func indexChanged(_ sender: Any) {
         switch segmentCtrl.selectedSegmentIndex {
-        case 0:
+        case 1:
+            break;
+        case 2:
+            scrollView.isHidden = true
+            tableView.isHidden = false
             break;
         default:
+            scrollView.isHidden = false
+            tableView.isHidden = true
             break;
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let comment = comments[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell") as? CommentTableViewCell {
+            cell.layoutMargins = UIEdgeInsets.zero
+            cell.configureCell(comment: comment)
+            return cell
+        }
+        
+        return UITableViewCell()
     }
     
     func getDataFromAPI(url: URL, onComplete: @escaping AgentDetailsAPIRequestCompletion) {
@@ -95,9 +134,21 @@ class AgentDetailsVC: UIViewController {
             response in
             let result = response.result
             if let dict = result.value as? Dictionary<String, AnyObject> {
-                if  let fairAgentData = dict["fairAgent"] as? Dictionary<String, AnyObject>  {
-                    onComplete(nil, fairAgentData)
+                
+                let fairAgent = FairAgentMapper.mapFromAPI(data: dict["fairAgent"] as AnyObject, existingAgent: self._agent)
+                var commentsResult = [Comment]()
+                
+                if let comments = dict["comments"] as? Array<AnyObject> {
+                    for comment in comments {
+                        let dateSubmitted = Date.FromISOString(dateString: comment["dateSubmitted"] as! String)
+                        let situation = comment["situation"] as! String
+                        let situationOtherDetails = comment["situationOtherDetails"] as! String
+                        let commentText = "\"\((comment["comments"] as! String).trimmingCharacters(in: .whitespacesAndNewlines))\""
+                        commentsResult.append(Comment(situation: situation, situationOtherDetails: situationOtherDetails, comments: commentText, dateSubmitted: dateSubmitted))
+                    }
                 }
+                
+                onComplete(nil, AgentDetailsResult(fairAgent: fairAgent, comments: commentsResult))
             }
         }
     }
@@ -107,7 +158,7 @@ class AgentDetailsVC: UIViewController {
         indicator.style = UIActivityIndicatorView.Style.large
         indicator.center = self.view.center
         self.view.addSubview(indicator)
-
+        
         indicator.startAnimating()
         indicator.backgroundColor = .white
     }
